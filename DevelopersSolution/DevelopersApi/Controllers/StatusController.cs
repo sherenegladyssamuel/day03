641434aeab79b81803384ad344e545f8d5625fb9
sherenegladyssamuel <116015523+sherenegladyssamuel@users.noreply.github.com>
2022-10-19 11:38:18 -0400

    Initial commit

---
+++ DevelopersSolution/DevelopersApi/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevelopersApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public ActionResult GetStatus()
+    {
+
+        var response = new StatusResponse("Looks Good, Captain!", DateTime.Now);
+        return Ok(response);
+    }
+}
+
+public record StatusResponse(string Status, DateTime whenChecked);
---
+++ docker-demos/demo2/src/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("I'm running in a container in an app built with C and I do not even need a distribution!\n");
+	printf("Now With New Features MULTILINE (note this is paid feature!)\n");
+	return (0);
+}
